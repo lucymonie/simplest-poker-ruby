9ee1bf3a2d03303b4c982bf09ca5907d56b72c56
lucymonie <lucy.monie.hall@gmail.com>
2017-07-04 19:20:07 +0100

    Initial commit of game files in ruby

---
+++ index.rb
@@ -0,0 +1,102 @@
+# require 'sinatra'
+
+class Game
+	def initialize (num_cards, num_players)
+		@num_cards = num_cards
+		@num_players = num_players
+		@deck = %w{AH 2H 3H 4H 5H 6H 7H 8H 9H XH JH QH KH AC 2C 3C 4C 5C 6C 7C 8C 9C XC JC QC KC AD 2D 3D 4D 5D 6D 7D 8D 9D XD JD QD KD AS 2S 3S 4S 5S 6S 7S 8S 9S XS JS QS KS}
+		@score = {"A"=>1, "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9, "X"=>10, "J"=>11, "Q"=>12, "K"=>13}
+	end
+
+	attr_reader :num_cards
+	attr_reader :num_players
+	attr_reader :deck
+	attr_reader :score
+
+	def get_random_number
+		deck_length = @deck.length
+		random_num = rand(deck_length)
+	end
+
+	def get_card (random_num)
+		@deck[random_num]
+	end
+
+	def remove_card_from_deck (card)
+		@deck -= [card]
+	end
+
+	def deal_hand
+		hand = Array.new
+		@num_cards.times do
+			card = get_card(get_random_number)
+			hand << card
+			remove_card_from_deck(card)
+		end
+		hand
+	end
+
+	def get_all_cards
+		all_hands = Hash.new
+		n = 1
+		@num_players.times do
+			all_hands["player#{n}"] = deal_hand
+			n += 1
+		end
+		all_hands
+	end
+
+	def get_score (cards)
+		score = 0
+		cards.each do |card|
+			measure = card[0]
+			score += @score[measure]
+		end
+		score
+	end
+
+	def get_all_scores (all_hands)
+		scores = Hash.new
+		all_hands.each do |player|
+			scores[player[0]] = get_score(player[1])
+		end
+		scores
+	end
+
+	def get_winner (all_scores)
+		winning_score = 0
+		winners = Array.new
+		all_scores.each do |player_score|
+			if player_score[1] > winning_score
+				winner = player_score[0]
+				winning_score = player_score[1]
+				winners = Array.new(1,player_score[0])
+			elsif player_score[1] == winning_score
+				winners << player_score[0]
+				winning_score = player_score[1]
+			end
+		end
+		winners
+	end
+
+	def get_winner_message (winner)
+		if winner.length == 1
+		  return "The winner is #{winner[0]}"
+		else
+			winner_message = "The winners are: "
+		  winner.each do |winner|
+			  winner_message += "#{winner}, "
+			end
+			winner_message.chop(2)
+		end
+	end
+
+end
+
+def play_game (num_players, num_cards)
+	game = Game.new(num_players, num_cards)
+	all_hands = game.get_all_cards
+	all_scores = game.get_all_scores (all_hands)
+	winner = game.get_winner (all_scores)
+	message = game.get_winner_message(winner)
+end
---
+++ spec/poker_game_tests.rb
@@ -0,0 +1,105 @@
+# may be set to 'development' but setting to test to run rspec
+ENV['RACK_ENV'] = 'test'
+
+$:.unshift File.join(File.dirname(__FILE__))
+require_relative File.join(File.dirname(__FILE__), "../index")
+
+# require testing gems
+require 'rspec'
+require 'rack/test'
+
+describe 'Simple Poker Game' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "can establish a new game with the correct number of cards and players" do
+    game = Game.new(6, 2)
+    cards = game.num_cards
+    players = game.num_players
+    expect(cards).to eq(6)
+    expect(players).to eq(2)
+  end
+
+  it "can get a random number" do
+    game = Game.new(6, 2)
+    500.times do
+      num = game.get_random_number
+      expect(num).to be >= 0
+      expect(num).to be <= 52
+    end
+  end
+
+  it "can get a random card from the deck" do
+    game = Game.new(6, 2)
+    num = 5
+    card = game.get_card(num)
+    expect(card).to eq('6H')
+  end
+
+  it "can remove the card selected from the deck" do
+    game = Game.new(5, 3)
+    num = game.get_random_number
+    card = game.get_card(num)
+    game.remove_card_from_deck(card)
+    deck = game.deck
+    expect(deck).not_to include(card)
+  end
+
+  it "can deal a hand of cards" do
+    game = Game.new(5, 2)
+    deck_length_before_deal = game.deck.length
+    hand = game.deal_hand
+    expect(hand.length).to eq(5)
+    expect(game.deck.length).to be < deck_length_before_deal
+    expect(game.deck.length).to eq(47)
+  end
+
+  it "can keep a record of each players hand" do
+    game = Game.new(6, 3)
+    all_players = game.get_all_cards
+    expect(all_players.length).to eq(3)
+    expect(all_players["player1"].length).to eq(6)
+  end
+
+  it "can give a score based on the cards in a given hand" do
+    game = Game.new(2, 6)
+    player1 = ["2D", "XC", "4D", "XD", "5S", "QD"]
+    score = game.get_score(player1)
+    expect(score).to eq(43)
+  end
+
+  it "can record scores for each player" do
+    game = Game.new(3, 6)
+    cards = {"player1"=>["2D", "XC", "4D", "XD", "5S", "QD"], "player2"=>["7H", "5H", "2C", "KD", "9D", "7S"], "player3"=>["3H", "9S", "6D", "6C", "4H", "7C"]}
+    scores = game.get_all_scores(cards)
+  end
+
+  it "can use the scores to determine the winner or winners" do
+    game = Game.new(3, 6)
+    cards = {"player1"=>["2D", "XC", "4D", "XD", "5S", "QD"], "player2"=>["7H", "5H", "2C", "KD", "9D", "7S"], "player3"=>["3H", "9S", "6D", "6C", "4H", "7C"]}
+    scores = game.get_all_scores(cards)
+    winners = game.get_winner(scores)
+    expect(winners) =~ ["player1", "player2"]
+  end
+
+  # Better way might be to have one function to determine the highest score
+  # Then have a function that selects any props that have this score
+
+  it "can play a full game, taking in the number of players and cards" do
+    winner_message = play_game(6, 4)
+    expect(winner_message).to include('winner')
+  end
+
+  # it "responds with success" do
+  #   get '/'
+  #   expect(last_response).to be_ok
+  # end
+  #
+  # it "says hello" do
+  #   get '/'
+  #   expect(last_response.body).to eq('Hello World')
+  # end
+end
